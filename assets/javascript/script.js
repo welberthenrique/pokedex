3202c44fefd38732684df3a5fa78f5f4a38d1e73
welberthenrique <welberthenrique@gmail.com>
2022-08-10 19:07:13 -0300

    Adicionado script javascript integrado com API pokemon

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector("pokemon_image");
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+
+  const data = await APIResponse.json();
+  return data;
+};
+
+const renderPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon);
+
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+  pokemonImage.src =
+    data["master"]["sprites"]["pokemon"]["versions"]["generation-v"][
+      "black-white"
+    ]["animated"]; //["front_default"];
+};
+renderPokemon("1");
+
+//sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif
